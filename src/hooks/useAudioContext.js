ff1f53a91d36d7bcf5e963a8ba67af6917b1e38a
sunghyun <sunghyun1148@gmail.com>
2025-02-06 17:43:17 +0900

    panner 및 이퀄라이저 필터 생성 추가

--- src/hooks/useAudioContext.js
+++ src/hooks/useAudioContext.js
@@ -1,15 +1,20 @@
 import { useEffect, useRef, useState } from "react";
 import { useMusicList } from "../context/MusicProvider";
+// Web Audio API를 이용한 오디오 시각화: https://developer.mozilla.org/ko/docs/Web/API/Web_Audio_API/Visualizations_with_Web_Audio_API
 
-// useAudioContext.js: AudioContext 및 AnalyserNode를 관리하는 커스텀 훅
+const EQ_FREQUENCIES = [60, 170, 310, 600, 1000, 3000, 6000]; // 주파수 음역대
+
+// # Web Audio API를 사용한 코드
 export default function useAudioContext(audioElement) {
   const audioSourceRef = useRef(null); // 오디오 컨텍스트 소스(MediaElementAudioSourceNode) 저장
 
   const [audioContext, setAudioContext] = useState(null); // AudioContext 저장
   const [analyser, setAnalyser] = useState(null); // AnalyserNode 저장
+  const [panner, setPanner] = useState(null);
+  const [filters, setFilters] = useState(EQ_FREQUENCIES);
   const { current } = useMusicList(); // 현재 재생 중인 음악이 있는지 확인용 Context 상태
 
-  // AudioContext 및 AnalyserNode를 생성하는 함수
+  // 오디오 컨텍스트, analyserNode, StereoPannerNode, 이퀄라이저 필터 생성하는 함수
   const initAudioContext = () => {
     // audioContext가 존재하지 않을 경우에만 실행
     if (!audioContext) {
@@ -18,42 +23,64 @@ export default function useAudioContext(audioElement) {
 
       // 2. AnalyserNode 생성
       const analyserNode = context.createAnalyser();
+      const pan = new StereoPannerNode(context, { pan: 0 });
       analyserNode.fftSize = 512; // FFT 크기를 설정 (시각화의 해상도에 영향을 줌) : fftSize 속성은 unsigned long 값이고 주파수 영역 데이터를 얻기 위해 고속 푸리에 변환(FFT)을 수행할 때 사용될 샘플에서의 window 사이즈를 나타낸다.
 
-      // 3. 생성된 AudioContext와 AnalyserNode를 상태에 저장
+      // 3. 이퀄라이저 필터 생성
+      const filterArr = EQ_FREQUENCIES.map((freq) => {
+        const filter = context.createBiquadFilter(); //
+        filter.type = "peaking"; // 피킹 필터 사용
+        filter.frequency.value = freq;
+        filter.Q.value = 1; // Q 값 조정 가능
+        filter.gain.value = 0; // 초기값 (dB)
+        return filter;
+      });
+
+      // 4. 오디오 컨텍스트 및 오디오 노드들 상태 저장
       setAudioContext(context);
       setAnalyser(analyserNode);
+      setPanner(pan);
+      setFilters(filterArr);
     }
   };
 
-  // 음악을 선택한 상태에서 audioContext와 analyser가 없는 경우 AudioContext 및 AnalyserNode를 초기화하는 effect -> 사용자 상호작용 없이 AudioContext 생성 불가능하므로 현재 음악 선택 후 AudioContext 생성하도록 함.
+  // 음악을 선택한 상태에서 audioContext와 analyser가 없는 경우 AudioContext 및 AnalyserNode를 초기화하는 effect -> 사용자 상호작용 없이 AudioContext 생성 혹은 활성화 불가능하므로 현재 음악 선택 후 AudioContext 생성하도록 함.
   useEffect(() => {
     if (current && !audioContext && !analyser) {
       initAudioContext();
     }
   }, [audioContext, analyser, current]);
 
-  // AudioContext와 AnalyserNode를 Audio 요소에 연결
+  // 생성된 오디오 노드들 연결하는 effect
   useEffect(() => {
-    if (audioContext && analyser && audioElement.current) {
-      // audioSourceRef(MediaElementAudioSourceNode)가 초기화되지 않은 경우 -> 오디오 컨텍스트 소스가 생성되지 않은 경우 실행
+    if (audioContext && analyser && panner && filters) {
+      // 오디오 소스 노드가 생성되지 않은 경우 실행
       if (!audioSourceRef.current) {
-        // 1. 오디오 컨텍스트 소스(MediaElementAudioSourceNode) 생성
+        // 1. 오디오 소스 노드(MediaElementAudioSourceNode) 생성
         const source = audioContext.createMediaElementSource(audioElement.current);
 
-        // 2. 분석될 오디오 소스(MediaElementAudioSourceNode)에 분석기(AnalyserNode) 연결
-        source.connect(analyser);
-        // 3. analyser에 AudioContext.detination(오디오 컨텍스트 종착점 -> 보통 스피커를 가리킴) 연결
-        analyser.connect(audioContext.destination);
+        // 2. 이퀄라이저 필터들을 체인으로 연결
+        source.connect(filters[0]); // 첫 번째 필터에 오디오 소스 연결
+        for (let i = 0; i < filters.length - 1; i++) {
+          filters[i].connect(filters[i + 1]); // 각 필터를 연결
+        }
 
-        // 4. 생성된 MediaElementAudioSourceNode를 Ref에 저장
+        // 3. 마지막 필터를 analyser 및 panner에 연결 후 출력 장치에 연결
+        filters[filters.length - 1].connect(analyser);
+        analyser.connect(panner);
+        panner.connect(audioContext.destination);
+
+        // 4. Ref에 오디오 소스 저장(중복 생성 방지)
         audioSourceRef.current = source;
       }
     }
-  }, [audioContext, analyser, audioElement]);
+  }, [audioContext, analyser, panner, filters]);
 
   return {
-    // analyser를 반환하여 오디오 시각화 및 분석에 사용
     analyser,
+    panner,
+    audioContext,
+    // audioSourceRef,
+    filters,
   };
 }
