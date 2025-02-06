7e90103a518f2757383bc570569d37ba81fe9416
sunghyun <sunghyun1148@gmail.com>
2025-02-06 17:47:39 +0900

    props 수정

--- src/components/Controller.jsx
+++ src/components/Controller.jsx
@@ -1,19 +1,21 @@
-import { useEffect } from "react";
-import { useMusicList } from "../context/MusicProvider";
+import { useEffect, useState } from "react";
 import style from "./Controller.module.css";
+import toast, { Toaster } from "react-hot-toast";
 
 function Controller({
   audioElement,
   setLoop,
   loop,
-  setVolumeBtn,
-  volumeBtn,
-  setVolume,
-  volume,
   playPause,
   setCurrentTime,
+  musics,
+  setCurrent,
+  current,
+  setCurrentIndex,
+  currentIndex,
 }) {
-  const { musics, setCurrent, current, setCurrentIndex, currentIndex } = useMusicList();
+  const [volumeBtn, setVolumeBtn] = useState(false);
+  const [volume, setVolume] = useState(50);
 
   // 재생, 정지 버튼 핸들러
   const handlePlayPause = () => {
@@ -28,7 +30,7 @@ function Controller({
   const handlePrevMusic = () => {
     // 노래가 0초대라면 이전 곡으로
     if (Math.floor(audioElement.current.currentTime) === 0) {
-      setCurrentIndex((n) => (n - 1 < 0 ? musics.length - 1 : n - 1));
+      setCurrentIndex((n) => (n - 1 < 0 ? musics?.length - 1 : n - 1));
 
       // 0초 초과면 노래 시작으로
     } else {
@@ -40,13 +42,13 @@ function Controller({
   const handleNextMusic = () => {
     // 기본, 루프 설정인 경우 다음 버튼 클릭 시 다음 노래로 넘어감
     if (loop === 0 || loop === 1) {
-      setCurrentIndex((n) => (n + 1) % musics.length);
+      setCurrentIndex((n) => (n + 1) % musics?.length);
 
       // 셔플 설정인 경우 다음 버튼 클릭 시 셔플
     } else if (loop === 2) {
       let random = currentIndex;
       while (random === currentIndex) {
-        random = Math.floor(Math.random() * musics.length);
+        random = Math.floor(Math.random() * musics?.length);
       }
       setCurrentIndex(random);
       setCurrent(musics[random]);
@@ -55,22 +57,43 @@ function Controller({
 
   useEffect(() => {
     // 곡(인덱스) 변경 시 현재 재생 곡(current) 업데이트
-    setCurrent(musics[currentIndex]);
+    if (musics) {
+      setCurrent(musics[currentIndex]);
 
-    // 선택한 곡이 없거나 삭제되면 play 버튼 렌더링, 곡 선택시 자동 재생과 pause 버튼 렌더링
-    if (!current) {
-      audioElement?.current?.pause();
-      setCurrentTime(0);
+      // 선택한 곡이 없거나 삭제되면 play 버튼 렌더링, 곡 선택시 자동 재생과 pause 버튼 렌더링
+      if (!current) {
+        audioElement?.current?.pause();
+        setCurrentTime(0);
+      }
     }
   }, [currentIndex, current]);
 
   return (
     <div className={style.controls}>
       <span
-        onClick={() => setLoop((prev) => (prev + 1) % 3)}
+        onClick={() => {
+          setLoop((prev) => (prev + 1) % 3);
+          // if (loop === 0) {
+          //   toast("한곡 반복");
+          // } else if (loop === 1) {
+          //   toast("셔 플");
+          // } else if (loop === 2) {
+          //   toast("다음 곡 재생");
+          // }
+        }}
         id="repeat-plist"
         className="material-symbols-outlined"
       >
+        {/* <Toaster
+          toastOptions={{
+            className: "",
+            style: {
+              "-webkit-text-fill-color": "#000",
+              fontSize: "20px",
+            },
+            duration: 2000,
+          }}
+        /> */}
         {loop === 0 ? "trending_flat" : loop === 1 ? "repeat_one" : "shuffle"}
       </span>
 
